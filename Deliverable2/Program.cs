00325b7dbc826ac3da99b3d31ff7afd349a19ff7
Trent Nedbal <Nedbal.trent@gmail.com>
2021-05-06 23:02:08 -0400

    deliverable2

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userResponse;
+            string pastResponse = null;
+            do
+            {
+                Console.WriteLine("What would you like to say to the bot?");
+
+                Console.WriteLine("hello");
+                Console.WriteLine("bye");
+                Console.WriteLine("sup");
+                Console.WriteLine("hello there");
+
+                userResponse = Console.ReadLine().ToLower();
+
+                if (userResponse == pastResponse)
+                {
+                    Console.WriteLine("I'm sorry but you have already said that"); 
+                }
+                else if (userResponse == "hello")
+                {
+                    Console.WriteLine("Hi good to see you");
+                    pastResponse = "hello";
+                }
+                else if (userResponse == "bye")
+                {
+                    Console.WriteLine("Goodbye");
+                }
+                else if (userResponse == "sup")
+                {
+                    Console.WriteLine("I am good");
+                    pastResponse = "sup";
+                }
+                else if (userResponse == "hello there")
+                {
+                    Console.WriteLine("General Kenobi");
+                    pastResponse = "hello there";
+                }
+            } while (userResponse != "bye");
+        }
+    }
+}
